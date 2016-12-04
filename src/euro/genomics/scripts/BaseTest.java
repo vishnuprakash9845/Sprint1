df849f65f632fdf7745011712fb27b758d2c5a31
VISHNU PRAKASH <vishnuprakash9845@gmail.com>
2016-12-04 21:10:57 +0530

    Modifying the BaseTest for Multiple System Execution(Selenium Grid)

--- src/euro/genomics/scripts/BaseTest.java
+++ src/euro/genomics/scripts/BaseTest.java
@@ -1,23 +1,66 @@
 package euro.genomics.scripts;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
 
 public abstract class BaseTest implements AutoConst{
 
 	public WebDriver driver;
 	
-	@BeforeMethod
-	public void preCondition()
+	
+	//Old Code
+/*	public void preCondition()
 	{
 		driver=new FirefoxDriver();
 		driver.get("http://localhost");
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-	}
+	}*/
+	
+	/*//New Code-1
+	public void preCondition() throws MalformedURLException
+	{
+		URL ra=new URL("http://192.168.1.4:4444/wd/hub");
+		DesiredCapabilities dc=new DesiredCapabilities();
+		//dc.setBrowserName("firefox");
+		//dc.setBrowserName("internet explorer");
+		dc.setBrowserName("chrome");
+		//dc.setVersion("2.20");
+		dc.setPlatform(Platform.WINDOWS);
+		driver=new RemoteWebDriver(ra,dc);
+		
+		//driver=new FirefoxDriver();
+		driver.get("http://localhost");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	}*/
+	
+	//New Code-2
+	@Parameters({"remoteURL","browser"})
+	@BeforeMethod
+		public void preCondition(String remoteURL,String browser) throws MalformedURLException
+		{
+			URL ra=new URL(remoteURL);
+			DesiredCapabilities dc=new DesiredCapabilities();
+			dc.setBrowserName(browser);
+			//dc.setVersion("2.20");
+			dc.setPlatform(Platform.WINDOWS);
+			driver=new RemoteWebDriver(ra,dc);
+			
+			//driver=new FirefoxDriver();
+			driver.get("http://localhost");
+			driver.manage().window().maximize();
+			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		}
 	
 	@AfterMethod
 	public void postCondition()
